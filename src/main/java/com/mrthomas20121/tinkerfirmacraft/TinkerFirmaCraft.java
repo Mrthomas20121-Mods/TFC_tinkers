8c4c33f73519d04629ac71dd951f2fb95ed54b8c
mrthomas20121 <thomas77300@gmail.com>
2019-12-01 16:02:30 +0100

    Bumped version to 1.0.2

--- src/main/java/com/mrthomas20121/tinkerfirmacraft/TinkerFirmaCraft.java
+++ src/main/java/com/mrthomas20121/tinkerfirmacraft/TinkerFirmaCraft.java
@@ -24,7 +24,7 @@ public class TinkerFirmaCraft
     public static TinkerFirmaCraft instance;
     public static final String MODID = "tinkerfirmacraft";
     public static final String NAME = "TinkerFirmaCraft";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public static Logger logger;
 
