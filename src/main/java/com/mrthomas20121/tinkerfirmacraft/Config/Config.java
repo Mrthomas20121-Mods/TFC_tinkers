7f1967608f15fab56aa83caffaf335faccaeb1f7
mrthomas20121 <thomas77300@gmail.com>
2019-12-01 16:01:39 +0100

    Fixed a config bug

--- src/main/java/com/mrthomas20121/tinkerfirmacraft/Config/Config.java
+++ src/main/java/com/mrthomas20121/tinkerfirmacraft/Config/Config.java
@@ -16,9 +16,12 @@ public class Config {
     public static boolean blue_steel = true;
     public static boolean roseGold = true;
     public static boolean sterlingSilver = true;
+    public static boolean wrought_iron = true;
     public static boolean blackBronze = true;
     public static boolean bismuthBronze = true;
     public static boolean bismuth = true;
+    public static boolean platinum = true;
+    public static boolean zinc = true;
 
     // Call this from CommonProxy.preInit(). It will create our config if it doesn't
     // exist yet and read the values if it does exist.
@@ -42,7 +45,15 @@ public class Config {
         // cfg.getBoolean() will get the value in the config if it is already specified there. If not it will create the value.
         black_steel = cfg.getBoolean("black_steel", CATEGORY_METERIALS, black_steel, "Set to false to disable black steel");
         red_steel = cfg.getBoolean("red_steel", CATEGORY_METERIALS, red_steel, "Set to false to disable red steel");
-        // yourRealName = cfg.getString("realName", CATEGORY_GENERAL, yourRealName, "Set your real name here");
+        blue_steel = cfg.getBoolean("red_steel", CATEGORY_METERIALS, blue_steel, "Set to false to disable blue steel");
+        roseGold = cfg.getBoolean("rose_gold", CATEGORY_METERIALS, roseGold, "Set to false to disable rose gold");
+        sterlingSilver = cfg.getBoolean("sterling_silver", CATEGORY_METERIALS, sterlingSilver, "Set to false to disable sterling silver");
+        wrought_iron = cfg.getBoolean("wrought_iron", CATEGORY_METERIALS, wrought_iron, "Set to false to disable wrought iron");
+        blackBronze = cfg.getBoolean("black_bronze", CATEGORY_METERIALS, blackBronze, "Set to false to disable black bronze");
+        bismuthBronze = cfg.getBoolean("bismuth_bronze", CATEGORY_METERIALS, bismuthBronze, "Set to false to disable bismuth bronze");
+        bismuth = cfg.getBoolean("bismuth", CATEGORY_METERIALS, bismuth, "Set to false to disable bismuth");
+        platinum = cfg.getBoolean("platinum", CATEGORY_METERIALS, platinum, "Set to false to disable platinum");
+        zinc = cfg.getBoolean("zinc", CATEGORY_METERIALS, zinc, "Set to false to disable zinc");
     }
     private static void initGeneralConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General Stuff");
