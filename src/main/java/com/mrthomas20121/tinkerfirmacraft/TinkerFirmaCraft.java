2af4159a3edfd45e5e72be7e4a1cf66670659d93
mrthomas20121 <thomas77300@gmail.com>
2019-12-10 09:30:21 +0100

    Bumped version to 1.0.3

--- src/main/java/com/mrthomas20121/tinkerfirmacraft/TinkerFirmaCraft.java
+++ src/main/java/com/mrthomas20121/tinkerfirmacraft/TinkerFirmaCraft.java
@@ -1,21 +1,16 @@
 package com.mrthomas20121.tinkerfirmacraft;
 
-import net.dries007.tfc.api.types.Metal;
-import net.dries007.tfc.objects.fluids.FluidsTFC;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.SidedProxy;
 import com.mrthomas20121.tinkerfirmacraft.proxy.CommonProxy;
-import slimeknights.tconstruct.library.MaterialIntegration;
-import slimeknights.tconstruct.library.TinkerRegistry;
-import slimeknights.tconstruct.library.materials.*;
-import slimeknights.tconstruct.library.utils.HarvestLevels;
-import slimeknights.tconstruct.tools.TinkerTraits;
 
 @Mod(modid = TinkerFirmaCraft.MODID, name = TinkerFirmaCraft.NAME, version = TinkerFirmaCraft.VERSION)
 public class TinkerFirmaCraft
@@ -24,7 +19,7 @@ public class TinkerFirmaCraft
     public static TinkerFirmaCraft instance;
     public static final String MODID = "tinkerfirmacraft";
     public static final String NAME = "TinkerFirmaCraft";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     public static Logger logger;
 
@@ -44,13 +39,11 @@ public class TinkerFirmaCraft
     public void init(FMLInitializationEvent event)
     {
         proxy.init(event);
-        logger.info("Loading TinkerFirmaCraft!");
         TConstructHelper.helper.init();
     }
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
         proxy.postInit(event);
-        TConstructHelper.helper.postInit();
     }
 }
