2ff3287b23021976e446a6ce0305626e47b88681
mrthomas20121 <thomas77300@gmail.com>
2020-03-25 11:37:59 +0100

    Bumped version to 1.2.1 and properly require mods.

--- src/main/java/com/mrthomas20121/tinkerfirmacraft/TinkerFirmaCraft.java
+++ src/main/java/com/mrthomas20121/tinkerfirmacraft/TinkerFirmaCraft.java
@@ -12,14 +12,18 @@ import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.SidedProxy;
 import com.mrthomas20121.tinkerfirmacraft.proxy.CommonProxy;
 
-@Mod(modid = TinkerFirmaCraft.MODID, name = TinkerFirmaCraft.NAME, version = TinkerFirmaCraft.VERSION)
+@Mod(modid = TinkerFirmaCraft.MODID, name = TinkerFirmaCraft.NAME, version = TinkerFirmaCraft.VERSION,
+        dependencies = "required-after:forge@[14.23.5.2847,);"
+        + "required-after:mantle@[1.12-1.3.3.49,);"
+        + "required-after:tconstruct@[1.12.2-2.12.0.157,);"
+        + "required-after:tfc@[0.29.4.119,);")
 public class TinkerFirmaCraft
 {
     @Mod.Instance
     public static TinkerFirmaCraft instance;
     public static final String MODID = "tinkerfirmacraft";
     public static final String NAME = "TinkerFirmaCraft";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
 
     public static Logger logger;
 
