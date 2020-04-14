3e5df7276abfaca3ef1affecc53f4fe583efc0e5
mrthomas20121 <thomas77300@gmail.com>
2020-04-14 11:31:27 +0200

    Changed the project name and bumed version to 1.2.6

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # TFC_Tinker's
 Adding tinker construct materials and tools(prospector pickaxe) to terrafirmacraft.
 
-curseforge link : https://www.curseforge.com/minecraft/mc-mods/tinkerfirmacraft
+curseforge link : https://www.curseforge.com/minecraft/mc-mods/tfctinkers
--- src/main/java/com/mrthomas20121/tfc_tinker/TFC_Tinker.java
+++ src/main/java/com/mrthomas20121/tfc_tinker/TFC_Tinker.java
@@ -21,7 +21,7 @@ public class TFC_Tinker
     public static TFC_Tinker instance;
     public static final String MODID = "tfc_tinker";
     public static final String NAME = "TFC_Tinker's";
-    public static final String VERSION = "1.2.5";
+    public static final String VERSION = "1.2.6";
 
     public static Logger logger;
 
