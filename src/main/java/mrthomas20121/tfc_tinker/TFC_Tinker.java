0752562a131c98f276705d64fc2daa7fd4052c4f
mrthomas20121 <thomas77300@gmail.com>
2020-06-17 11:45:01 +0200

    Bumped version to 1.3.1

--- src/main/java/mrthomas20121/tfc_tinker/TFC_Tinker.java
+++ src/main/java/mrthomas20121/tfc_tinker/TFC_Tinker.java
@@ -21,7 +21,7 @@ public class TFC_Tinker
     public static TFC_Tinker instance;
     public static final String MODID = "tfc_tinker";
     public static final String NAME = "TFC_Tinker's";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
 
     public static Logger logger;
 
