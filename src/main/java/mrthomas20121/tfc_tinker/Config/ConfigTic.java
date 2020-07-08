653175d546a5d728c808ca4ea416f5fe67c37d7f
mrthomas20121 <thomas77300@gmail.com>
2020-07-08 09:42:37 +0200

    Now use biolib 1.0.4 and conarm 1.12.2

--- src/main/java/mrthomas20121/tfc_tinker/Config/ConfigTic.java
+++ src/main/java/mrthomas20121/tfc_tinker/Config/ConfigTic.java
@@ -1,11 +1,17 @@
 package mrthomas20121.tfc_tinker.Config;
 
 import mrthomas20121.tfc_tinker.TFC_Tinker;
+import net.minecraftforge.common.config.Config;
 import mrthomas20121.tfc_tinker.proxy.CommonProxy;
+import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Level;
 
-public class Config {
+@Mod.EventBusSubscriber(modid = TFC_Tinker.MODID)
+public final class Config {
 
     private static final String CATEGORY_METERIALS = "materials";
     private static final String CATEGORY_GENERAL = "general";
@@ -14,15 +20,9 @@ public class Config {
     public static boolean black_steel = true;
     public static boolean red_steel = true;
     public static boolean blue_steel = true;
-    public static boolean roseGold = true;
-    public static boolean sterlingSilver = true;
     public static boolean wrought_iron = true;
     public static boolean blackBronze = true;
     public static boolean bismuthBronze = true;
-    public static boolean bismuth = true;
-    public static boolean nickel = true;
-    public static boolean platinum = true;
-    public static boolean zinc = true;
     public static boolean constantan = true;
     public static boolean tungsten = true;
     public static boolean tungstenSteel = true;
@@ -32,8 +32,16 @@ public class Config {
     public static boolean titanium = true;
     public static boolean invar = true;
     public static boolean nickel_silver = true;
-    public static boolean red_alloy = true;
     public static boolean tfctech = true;
+
+    @SubscribeEvent
+    public static void onConfigChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(TFC_Tinker.MODID))
+        {
+            ConfigManager.sync(TFC_Tinker.MODID, Config.Type.INSTANCE);
+        }
+    }
 
     // Call this from CommonProxy.preInit(). It will create our config if it doesn't
     // exist yet and read the values if it does exist.
@@ -58,15 +66,9 @@ public class Config {
         black_steel = cfg.getBoolean("black_steel", CATEGORY_METERIALS, black_steel, "Set to false to disable black steel");
         red_steel = cfg.getBoolean("red_steel", CATEGORY_METERIALS, red_steel, "Set to false to disable red steel");
         blue_steel = cfg.getBoolean("blue_steel", CATEGORY_METERIALS, blue_steel, "Set to false to disable blue steel");
-        roseGold = cfg.getBoolean("rose_gold", CATEGORY_METERIALS, roseGold, "Set to false to disable rose gold");
-        sterlingSilver = cfg.getBoolean("sterling_silver", CATEGORY_METERIALS, sterlingSilver, "Set to false to disable sterling silver");
         wrought_iron = cfg.getBoolean("wrought_iron", CATEGORY_METERIALS, wrought_iron, "Set to false to disable wrought iron");
         blackBronze = cfg.getBoolean("black_bronze", CATEGORY_METERIALS, blackBronze, "Set to false to disable black bronze");
         bismuthBronze = cfg.getBoolean("bismuth_bronze", CATEGORY_METERIALS, bismuthBronze, "Set to false to disable bismuth bronze");
-        bismuth = cfg.getBoolean("bismuth", CATEGORY_METERIALS, bismuth, "Set to false to disable bismuth");
-        nickel = cfg.getBoolean("nickel", CATEGORY_METERIALS, nickel, "Set to false to disable nickel");
-        platinum = cfg.getBoolean("platinum", CATEGORY_METERIALS, platinum, "Set to false to disable platinum");
-        zinc = cfg.getBoolean("zinc", CATEGORY_METERIALS, zinc, "Set to false to disable zinc");
         constantan = cfg.getBoolean("constantan", CATEGORY_METERIALS, constantan, "Set to false to disable constantan");
         tungsten = cfg.getBoolean("tungsten", CATEGORY_METERIALS, tungsten, "Set to false to disable tungsten");
         tungstenSteel = cfg.getBoolean("tungsten_steel", CATEGORY_METERIALS, tungstenSteel, "Set to false to disable tungsten_steel");
@@ -76,7 +78,6 @@ public class Config {
         osmium = cfg.getBoolean("osmium", CATEGORY_METERIALS, osmium, "Set to false to disable osmium");
         invar = cfg.getBoolean("invar", CATEGORY_METERIALS, invar, "Set to false to disable invar");
         nickel_silver = cfg.getBoolean("nickel_silver", CATEGORY_METERIALS, nickel_silver, "Set to false to disable Nickel Silver");
-        red_alloy = cfg.getBoolean("red_alloy", CATEGORY_METERIALS, red_alloy, "Set to false to disable Red Alloy");
         tfctech = cfg.getBoolean("tfc_tech_integration", CATEGORY_METERIALS, invar, "Set to false to disable tfctech integration");
     }
     private static void initGeneralConfig(Configuration cfg) {
