fda3eca3a1dfcebf29f690081be6dab9c7775303
mrthomas20121 <thomas77300@gmail.com>
2021-03-29 11:00:19 +0200

    Prospector Pick fix

--- src/main/java/mrthomas20121/tfc_tinker/compat/tinkers_construct/tool/ToolProspectorPick.java
+++ src/main/java/mrthomas20121/tfc_tinker/compat/tinkers_construct/tool/ToolProspectorPick.java
@@ -5,12 +5,8 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import mrthomas20121.tfc_tinker.compat.tinkers_construct.Parts;
-import net.dries007.tfc.api.events.ProspectEvent;
 import net.dries007.tfc.api.types.Metal;
-import net.dries007.tfc.network.PacketProspectResult;
 import net.dries007.tfc.objects.items.metal.ItemProspectorPick;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraftforge.common.MinecraftForge;
 import slimeknights.tconstruct.library.materials.HandleMaterialStats;
 import slimeknights.tconstruct.library.materials.HeadMaterialStats;
 import slimeknights.tconstruct.library.materials.MaterialTypes;
@@ -20,30 +16,15 @@ import slimeknights.tconstruct.library.tools.AoeToolCore;
 import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.tools.ToolNBT;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.*;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.dries007.tfc.ConfigTFC;
-import net.dries007.tfc.TerraFirmaCraft;
-import net.dries007.tfc.api.capability.player.CapabilityPlayerData;
-import net.dries007.tfc.api.types.Ore;
-import net.dries007.tfc.util.skills.ProspectingSkill;
-import net.dries007.tfc.util.skills.SkillType;
-import net.dries007.tfc.world.classic.worldgen.vein.VeinRegistry;
-import net.dries007.tfc.world.classic.worldgen.vein.VeinType;
+import slimeknights.tconstruct.library.utils.ToolHelper;
 import slimeknights.tconstruct.tools.TinkerTools;
-import net.dries007.tfc.objects.items.metal.ItemProspectorPick.ProspectResult.Type;
 
 public class ToolProspectorPick extends AoeToolCore {
-    private static final int COOLDOWN = 10;
-    private static final Random RANDOM = new Random();
-    private static final int PROSPECT_RADIUS = 12;
     public static final float DURABILITY_MODIFIER = 1.1f;
 
     public ToolProspectorPick() {
